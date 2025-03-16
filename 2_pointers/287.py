4a792d4dc284589b656acaa464338d01141b4e18
zlrkw11 <ray040406@gmail.com>
2025-03-16 21:53:37 +1300

    287 done

--- 2_pointers/287.py
+++ 2_pointers/287.py
@@ -1,15 +1,16 @@
 from typing import List
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        left = 0
-        right = len(nums)-1
-        while left < len(nums) and right > -1:
-            if nums[left] == nums[right]:
-                return nums[left]
-            left += 1
-            right -= 1
-
-        return 
+        d= {nums[i]:0 for i in range(len(nums))}
+    
+        for i in range(len(nums)):
+            if nums[i] in d:
+                d[nums[i]]+=1
+        
+        for key, value in d.items():
+            if value > 1:
+                return key
     
 solution = Solution()
 print(solution.findDuplicate([1,3,4,2,2]))
+print(solution.findDuplicate([3,3,3,3,3]))
