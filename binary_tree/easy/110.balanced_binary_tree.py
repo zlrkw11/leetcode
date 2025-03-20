acc50a6b27fbdc7fd0a9a28e311b7a519c9695fc
zlrkw11 <ray040406@gmail.com>
2025-03-20 17:02:41 +1300

    fix code p.110

--- binary_tree/easy/110.balanced_binary_tree.py
+++ binary_tree/easy/110.balanced_binary_tree.py
@@ -9,11 +9,12 @@ class TreeNode:
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def height(node):
-            if node == None:
-                return -1
-            left = height(node)
-            right = height(node)
+            if node is None:
+                return 0
+            left = height(node.left)
+            right = height(node.right)
             if left==-1 or right==-1 or abs(left-right)>1:
                 return -1
             return max(left, right)+1
-        return False
+
+        return height(root)>=0
