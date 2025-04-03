4147250632d7b9e25fbbca66f98a3e920c54561a
ZL <ray040406@gmail.com>
2025-04-04 01:27:07 +1300

    p.55 done

--- greedy/55.jump_game.py
+++ greedy/55.jump_game.py
@@ -1,4 +1,10 @@
 from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        t = len(nums)-1
+        mx = 0
+        for i,x in enumerate(nums):
+            if mx < i:
+                return False
+            mx = max(mx, i+x)
+        return True
+
