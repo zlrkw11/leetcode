552af89a22a3b1b9828d876a2d57609b43c775b5
ZL <ray040406@gmail.com>
2025-06-01 22:26:42 +1200

    feat: q17 hashmap

---
+++ hash_table/17.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        d = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+        ans = [""]
+        for num in digits:
+            s = d[int(num)-2]
+            ans = [a+b for a in ans for b in s]
+        return ans
