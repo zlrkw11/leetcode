7e8a5b692b8bdf40057df06fed4de34cefa9b360
ZL <ray040406@gmail.com>
2025-11-17 15:55:14 +1300

    feat: longest substring without repeating characters

--- math/longest_substring_without_repeating_characters.py
+++ math/longest_substring_without_repeating_characters.py
@@ -1,6 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        return 1
+        l = 0
+        charSet = set()
+        res = 0
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l+=1
+            charSet.add(s[r])
+            res = max(res, r-l+1)
+        return res
 
 
 s = Solution()
