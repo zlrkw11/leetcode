ed0a6beec971d3a8cd6bcd2890722a5b7df8294b
zl <ray040406@gmail.com>
2025-11-29 18:27:14 +1300

    feat: adjustment

--- sort/methods/merge_sort.py
+++ sort/methods/merge_sort.py
@@ -4,7 +4,7 @@ def merge_sort(nums):
     
     mid = len(nums)//2
     l = merge_sort(nums[:mid])
-    r = merge_srot(nums[mid:])
+    r = merge_sort(nums[mid:])
     return merge(l, r)
 
 def merge(l, r):
@@ -19,15 +19,16 @@ def merge(l, r):
             res.append(r[j])
             j+=1
 
-    while len(l):
+    while i < len(l):
         res.append(l[i])
         i += 1
 
-    while len(r):
+    while j < len(r):
         res.append(r[j])
         j+=1
     return res
 
         
-
+test = [12,32,20,77,80,15]
+print(merge_sort(test))
         
