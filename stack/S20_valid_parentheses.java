e795eed5f67e15e774365d9cc0345a4f8406f7c3
ZL <ray040406@gmail.com>
2025-12-23 18:38:00 +1300

    feat: solution 20 [stack]

---
+++ stack/S20_valid_parentheses.java
@@ -0,0 +1,29 @@
+package stack;
+
+import java.util.Stack;
+import java.util.HashMap;
+
+public class S20_valid_parentheses {
+
+    class Solution {
+        public boolean isValid(String s) {
+            HashMap<Character, Character> map = new HashMap<>();
+            Stack<Character> stk = new Stack<>();
+            map.put(')', '(');
+            map.put(']', '[');
+            map.put('}', '{');
+
+            for (int i = 0; i < s.length(); i++) {
+                if (map.containsKey(s.charAt(i))) {
+                    if (stk.isEmpty() || stk.pop() != map.get(s.charAt(i))) {
+                        return false;
+                    }
+                } else {
+                    stk.push(s.charAt(i));
+                }
+
+            }
+            return true;
+        }
+    }
+}
