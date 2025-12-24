b5d9e5018fd1f99696f51fcfead784f78ca9dc81
ZL <ray040406@gmail.com>
2025-12-24 16:32:39 +1300

    feat: sliding window [209]

--- sliding_window/S209_minimum_size_subarray_sum.java
+++ sliding_window/S209_minimum_size_subarray_sum.java
@@ -3,7 +3,22 @@ package sliding_window;
 public class S209_minimum_size_subarray_sum {
     class Solution {
         public int minSubArrayLen(int target, int[] nums) {
-            return 0;
+            int left = 0;
+            int right = 0;
+            int sum = 0;
+            int minLen = Integer.MAX_VALUE;
+
+            while (right < nums.length) {
+                sum += nums[right];
+                right++;
+                while (sum >= target) {
+                    minLen = Math.min(minLen, right - left);
+                    sum -= nums[left];
+                    left++;
+                }
+            }
+
+            return minLen;
         }
     }
 }
