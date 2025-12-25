709e1103609500cec650bdba5e79d3283a11d1b1
ZL <ray040406@gmail.com>
2025-12-25 19:35:17 +1300

    feat: q3475 [greedy]

---
+++ arrays/3075_maximize_happiness.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        total_sum = 0
+
+        for i in range(k):
+            curr = happiness[i] - i # minus 1 every round until K
+
+            if curr > 0:
+                total_sum += curr
+
+            else:
+                return 
+                    
+        return total_sum
---
+++ bfs/S200_number_of_islands.java
@@ -0,0 +1,9 @@
+package bfs;
+
+public class S200_number_of_islands {
+    class Solution {
+        public int numIslands(char[][] grid) {
+            return 0;
+        }
+    }
+}
