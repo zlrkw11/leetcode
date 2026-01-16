045c721520e6286fecebbdea521068dec7aae9e6
ZL <ray040406@gmail.com>
2026-01-17 02:57:28 +1300

    feat: hashmap [1]

--- grind-75/1_two_sum.py
+++ grind-75/1_two_sum.py
@@ -1,8 +1,7 @@
 from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-
+        d= {}
         for i in range(len(nums)):
             num = nums[i]
             comp = target - num
@@ -10,7 +9,4 @@ class Solution:
                 return [i, d[comp]]
             else:
                 d[num] = i
-            
-s = Solution()
-
-print(s.twoSum([2,7,11,15], 9))
+       
