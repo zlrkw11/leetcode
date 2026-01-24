a8b8e0a5561182876ad98da4d85af76d75d369d7
ZL <ray040406@gmail.com>
2026-01-24 17:55:35 +1300

    feat: binary search

---
+++ neetcode/704_binary_search.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+
+        #continue going until 0 possibilities left
+        while l <= r:
+            m=(l+r)//2
+            if nums[m] > target:
+                r = m-1
+            elif nums[l] > target:
+                l = m+1
+            else:
+                return m
+            
+        return -1
+
+    
+s=Solution()
+s.search([8,3,4,1], 4)
