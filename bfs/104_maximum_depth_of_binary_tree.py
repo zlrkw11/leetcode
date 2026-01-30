acc977ea3fa421ad9917cfb3339fa952bd8af857
ZL <ray040406@gmail.com>
2026-01-31 00:45:22 +1300

    feat: bfs max depth [104]

---
+++ bfs/104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        q = deque([root])
+        depth = 0
+
+        while q:
+            size = len(q)
+            for _ in range(size):
+                node = q.popleft()
+                
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            depth+=1
+
+        return depth
